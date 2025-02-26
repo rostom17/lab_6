b060af47e623e09ca22676cfde855fb9c7e396cf
rostom17 <rostom825@gmail.com>
2025-02-26 10:44:35 +0600

    Cached Image

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,8 +12,26 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("hello wrold"),
+      appBar: AppBar(
+        backgroundColor: Colors.greenAccent,
+        title: Center(child: Text("Learning Lab 6")),
+      ),
+      body: ListView(
+        children: [
+          Image.network(
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXQrpJH5YofrmGgo0Ct_wDyYu36LzTwZKADw&s",
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          CachedNetworkImage(
+            imageUrl:
+                "https://www.nbc.com/sites/nbcblog/files/styles/scale_600/public/2024/03/kung-fu-panda-4-1.jpg",
+            placeholder: (context, url) => CircularProgressIndicator(),
+            errorWidget: (context, url, error) => Icon(Icons.error),
+          ),
+        ],
       ),
     );
   }
