8edad2206f6549fe948a5838dab491afeca58be8
rostom17 <rostom825@gmail.com>
2025-02-26 10:59:28 +0600

    Drawer Learned

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -16,6 +16,60 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.greenAccent,
         title: Center(child: Text("Learning Lab 6")),
       ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              title: Text("Recent"),
+              leading: Icon(Icons.alarm),
+              onTap: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("Hello World"),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              title: Text("Image"),
+              leading: Icon(Icons.image),
+              onTap: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("Hello Image "),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              title: Text("Video"),
+              leading: Icon(Icons.movie),
+              onTap: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("Hello Ripon Video"),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              title: Text("Sd Card"),
+              leading: Icon(Icons.sd_card),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.eject),
+              ),
+              onTap: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("Open SD-card"),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
       body: ListView(
         children: [
           Image.network(
